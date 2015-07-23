51be6e2a28f680a1ec63e625d184438ec06ccb6e
master_j <masterj63@gmail.com>
2015-07-23 14:48:38 +0300

    some

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from sys import argv
+
 from filters import filter_by_quality
 
+
 # open file and read lines
-fastq_in = open('child10_S4_L001_R1_001.fastq', 'r')
+if len(argv) == 2:
+    file_name = argv[1]
+else:
+    file_name = 'child10_S4_L001_R1_001.fastq'
+    print('using default file_name: ' + file_name)
+
+fastq_in = open(file_name, 'r')
 fastq_in = fastq_in.readlines()
 
-fastq_out = open('out_child10_S4_L001_R1_001.fastq', 'w')
+fastq_out = open('out_' + file_name, 'w')
 
 
 def out_write(line):
@@ -15,10 +24,8 @@ def out_write(line):
 # iterate over the lines
 i = 0
 len_dist = {}
+max_read_len = 1
 while i < len(fastq_in):
-    # if i > 850:
-    #     break
-
     name = fastq_in[0 + i].strip()  # @name
     atgc = fastq_in[1 + i].strip()  # ATGC
     # name = fastq[2 + i].strip() # plus
@@ -26,6 +33,7 @@ while i < len(fastq_in):
     i += 4
 
     filtered_atgc, filtered_quals = filter_by_quality(atgc, quals)
+    max_read_len = max(max_read_len, len(filtered_atgc))
 
     out_write(name)
     out_write(filtered_atgc)
@@ -39,7 +47,8 @@ while i < len(fastq_in):
 
 fastq_out.close()
 
-for i in range(0, 302):
+print('quality distribution (from {} to {} inclusively):'.format(0, max_read_len))
+for i in range(0, 1 + max_read_len):
     # print(i)
     if i not in len_dist:
         print(0)
